ddfa54bd3e05893aba59496fac3c63dc6b49d1a8
Simona Peneva <peneva.simona@gmail.com>
2017-04-06 22:29:43 +0300

    mongoDB test1

--- routes/mongotest.js
+++ routes/mongotest.js
@@ -1,6 +1,7 @@
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('mongo ds151060.mlab.com:51060/social-network -u alen -p alen1234');
+var db = monk('mongodb://alen:alen1234@ds151060.mlab.com:51060/social-network');
 
 var users = db.get('users');
 users.insert({ name: "Zvezdelin", age: "3.14" });
+urers.find({}, {})
