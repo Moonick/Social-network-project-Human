70bab9089b86981de51d01bcf0d744409ba86634
Simona Peneva <peneva.simona@gmail.com>
2017-04-19 17:16:25 +0300

    infinite scroll working

--- public/javascripts/controllers/postController.js
+++ public/javascripts/controllers/postController.js
@@ -1,15 +1,12 @@
 app.controller('postController', function($scope, postService) {
     postService.downloadPosts().then(function(res) {
         $scope.posts = res.data;
+        $scope.data = $scope.posts.slice(0, 5);
+        $scope.loadMore = function() {
+            $scope.data = $scope.posts.slice(0, $scope.data.length + 5);
+        };
     });
 
-
-    $scope.loadMore = function() {
-        var last = $scope.images[$scope.images.length - 1];
-        for (var i = 1; i <= 8; i++) {
-            $scope.images.push(last + i);
-        }
-    };
     $scope.addPost = function() {
         var text = $('#create-post textarea')[0].value;
         var post = {
