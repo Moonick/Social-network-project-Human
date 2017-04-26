c2b0ac37a91d0374fa40634da82812aa1870c5e0
Alen-Nikolov <alennikolov95@gmail.com>
2017-04-26 11:36:39 +0300

    username says the names of the user

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,41 +1,33 @@
-$(function() {
+$(function () {
     $('#side-menu').metisMenu();
 
     function addImageBtnPost() {
-        $('#create-post .btnPicture').on('click', function() {
+        $('#create-post .btnPicture').on('click', function () {
             $('#create-post input[type=file]').click();
         });
     }
     setTimeout(addImageBtnPost, 500);
 
     function addImageBtnPhoto() {
-        $('#add-photo .btnPicture').on('click', function() {
+        $('#add-photo .btnPicture').on('click', function () {
             $('#add-photo input[type=file]').click();
         });
     }
     setTimeout(addImageBtnPhoto, 500);
-    $(window).on('hashchange', function(e) {
+    $(window).on('hashchange', function (e) {
         //execute code
         setTimeout(addImageBtnPhoto, 500);
         setTimeout(addImageBtnPost, 500);
     });
 
 
-    $.get("/user", function(data) {
-        // var img = document.createElement('img');
-        // img.style.width = "30px";
-        // img.style.height = "auto";
-        // img.src = data.profImgUrl;
-        var span = document.createElement('span');
-        span.innerText = capitalizeFirstLetter(data.fname) + " " + capitalizeFirstLetter(data.lname);
-        // $("#side-menu> li >a")[0].append(img);
-
-        $("ul.dropdown-user a")[0].text = capitalizeFirstLetter(data.fname) + " " + capitalizeFirstLetter(data.lname);
-        $("#side-menu>li>a")[0].append(span);
-
+    $.get("/user", function (data) {
         function capitalizeFirstLetter(str) {
             return str.charAt(0).toUpperCase() + str.slice(1);
         }
+        var username = $("#link-profile");
+        username.text(capitalizeFirstLetter(data.fname) + " " + capitalizeFirstLetter(data.lname));
+        // $("#side-menu> li >a")[0].append(img);
     });
 
 });
@@ -43,8 +35,8 @@ $(function() {
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
-$(function() {
-    $(window).bind("load resize", function() {
+$(function () {
+    $(window).bind("load resize", function () {
         var topOffset = 50;
         var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
         if (width < 768) {
@@ -66,7 +58,7 @@ $(function() {
     // var element = $('ul.nav a').filter(function() {
     //     return this.href == url;
     // }).addClass('active').parent().parent().addClass('in').parent();
-    var element = $('ul.nav a').filter(function() {
+    var element = $('ul.nav a').filter(function () {
         return this.href == url;
     }).addClass('active').parent();
 
