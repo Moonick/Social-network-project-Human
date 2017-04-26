77fb0eef131dacb7a42499400447371ee9eefd7b
Alen-Nikolov <alennikolov95@gmail.com>
2017-04-26 11:54:53 +0300

    Registration fixed so that it requires names to be at least 3 charachters

--- routes/register.js
+++ routes/register.js
@@ -13,6 +13,9 @@ router.post('/', urlencodedParser, function(req, res) {
     var users = db.get('users');
     var user = req.body;
     user.profileImageUrl = "http://enadcity.org/enadcity/wp-content/uploads/2017/02/profile-pictures.png";
+    if(user.fname.length<3 || user.lname.length<3){
+        res.render('register',{ error:"Your name must have at least 3 characters!"});
+    }
     users.find({ email: user.email }).then(function(data) {
         if (data.length == 0) {
             users.insert(user);
