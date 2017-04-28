f7a43e476aa7d25774d68bf94c0dd77776e4479b
Alen-Nikolov <alennikolov95@gmail.com>
2017-04-28 16:20:42 +0300

    page back fixed

--- routes/user.js
+++ routes/user.js
@@ -41,7 +41,6 @@ router.get('/posts', function(req, res) {
     var db = req.db;
     var posts = db.get('posts');
     var userID = req.session.user._id;
-    console.log("here");
 
     posts.find({ user_id: userID }, { sort: { date: -1 } }).then(function(posts) {
         res.json(posts);
