a75ae4769e162b1b4b30ceb125073bd730291d18
Alen-Nikolov <alennikolov95@gmail.com>
2017-04-30 13:18:40 +0300

    comments fixex

--- public/javascripts/controllers/commentController.js
+++ public/javascripts/controllers/commentController.js
@@ -1,13 +1,13 @@
-app.controller('commentController', ['$scope', '$rootScope', 'commentService', 'userService', function($scope, $rootScope, commentService, userService) {
+app.controller('commentController', ['$scope', '$rootScope', 'commentService', 'userService', function ($scope, $rootScope, commentService, userService) {
     var postId = $scope.$parent.data._id;
     var userId = $rootScope.user._id;
     var user = $rootScope.user;
 
     // ============== LOAD ALL COMMENTS ================
-    commentService.downloadComments(postId).then(function(res) {
+    commentService.downloadComments(postId).then(function (res) {
         $scope.comments = res.data;
         // =========== ADD NEW COMMENT ==============
-        $scope.addComment = function(event) {
+        $scope.addComment = function (event) {
             var comment = {
                 text: $scope.commentText,
                 postId: postId,
@@ -18,21 +18,20 @@ app.controller('commentController', ['$scope', '$rootScope', 'commentService', '
                 likes: [],
                 date: new Date().toLocaleString()
             };
-            if ($('.panel-comments textarea').val() !== "") {
-                commentService.addComment(comment).then(function(res) {
+            if (comment.text !== "") {
+                commentService.addComment(comment).then(function (res) {
                     if (res.status == 201) {
                         $scope.comments.push(res.data);
                         $('.panel-comments textarea').val("");
-                        $scope.$parent.numOfComments()
-
+                        $scope.$parent.numOfComments();
+                        $scope.commentText="";
                     }
                 });
             }
-
         };
     });
 
-    userService.getCurrentUser().then(function(res) {
+    userService.getCurrentUser().then(function (res) {
         $scope.user = res.data;
     });
 
